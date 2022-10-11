346e48f4d366809007049155a673fb13b6d81a2b
Belkbh <belkbh@gmail.com>
2022-10-11 19:32:50 -0400

    Got basics up

---
+++ routes/api/user-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.get('/', async (req, res) => {
+  //get all users
+});
+
+router.get('/', async (req, res) => {
+  //get all users
+});
+
+
+
+
+
+
+//////////////////
+
+router.get('/:userId/friends/:friendId', async (req, res) => {
+  //get all users
+})
